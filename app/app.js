6451b3abaac2631287c2ef854d64236531781e0f
BennCon <benjamin.consterdine@gmail.com>
2021-07-23 18:13:38 +0100

    Implemented basic authentication with Auth0

--- app/app.js
+++ app/app.js
@@ -4,6 +4,8 @@ const app = express();
 const path = require('path');
 const glob = require( 'glob' )
 const mongoose = require('mongoose');
+const { auth } = require('express-openid-connect');
+
 
 
 app.use(express.static(path.join(__dirname, '/public')));
@@ -22,14 +24,26 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 //DB Setup
-const url = ''
-mongoose.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
-    if (err) return console.error(err);
-    console.log('Connected to Database');
-  })
+// const url = ''
+// mongoose.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+//     if (err) return console.error(err);
+//     console.log('Connected to Database');
+//   })
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: 'a long, randomly-generated string stored in env',
+    baseURL: 'http://localhost:3000',
+    clientID: 'G87Fz1Qml6T2QpiRJCiwL1wP0l1LG7Z2',
+    issuerBaseURL: 'https://dev-w189a8dc.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
 
 //Port
-const port = process.env.PORT || 5050;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on ${port}`));
 
 module.exports = app;
---
+++ app/routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+app.set('view engine', 'ejs');
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  });
+
+
+module.exports = app;
+
+// const { requiresAuth } = require('express-openid-connect');
+
+// app.get('/profile', requiresAuth(), (req, res) => {
+//   res.send(JSON.stringify(req.oidc.user));
+// });
