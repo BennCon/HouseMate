8ca9f6a1dbe85ad35bcec34e34265bef71fef574
BennCon <benjamin.consterdine@gmail.com>
2021-07-28 20:54:22 +0100

    Added bill helper class

---
+++ app/helpers/Bill.js
@@ -0,0 +1,9 @@
+class Bill {
+    constructor(name, amount, type, date) {
+        this.name = name,
+        this.amount = amount,
+        this.type = type,
+        this.date = date
+    };  
+
+}
